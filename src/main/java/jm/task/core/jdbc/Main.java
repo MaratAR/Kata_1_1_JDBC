4ee45aa73fec643cc8776dbc306fd9a968eee767
Marat Askarov <Zamgar240@gmail.com>
2023-11-08 12:45:12 +0300

    Hibernate task commit 4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,13 +2,9 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        Util.getConnection();
+    public static void main(String[] args) {
 
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
